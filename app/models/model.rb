d181b25adcfd5612fea0055b7e7cff3418eb5ed9
krtb <kurtbauer55@protonmail.com>
2018-06-25 19:37:20 -0400

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,6 +1,6 @@
 class Model
 
-end
+
 
 # logic of our app
 # represent component of app (user, post, comment)
@@ -10,5 +10,5 @@ end
 
 # Models - represent teh data and object logic of our if __FILE__ == $PROGRAM_NAME
 
-  
+
 end
